a757033c1109fe9ffe2a52747bf020082fbe84a5
ylcdolphin <33015665+ylcdolphin@users.noreply.github.com>
2024-12-22 17:30:59 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 10)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     } else if (receivedNumber == 2) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 10)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
     } else if (receivedNumber == 3) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 10)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
     } else if (receivedNumber == 4) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 10)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
     }
 })
 radio.setGroup(1)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 10)
+basic.pause(1000)
+maqueen.motorStop(maqueen.Motors.All)
